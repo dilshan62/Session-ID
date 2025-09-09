f27551a5e1bd0d2cd3bfe78e9cd92d994b66c18d
dilshan62 <dilshanchanushka.md@gmail.com>
2025-09-09 06:09:02 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: 'dinithihansika21865@gmail.com',
-    password: 'AdanukaZ@123',
+    email: 'danukadanuka21865@gmail.com',
+    password: 'dilshan$123',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
